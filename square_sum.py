b61cbecc6fd4e85ebf9319fc6180fcacdae1afc1
lwrig <lwrigle@gmail.com>
2021-05-08 10:04:05 -0400

    some problems finished

--- square_sum.py
+++ square_sum.py
@@ -1,17 +1,3 @@
-def duplicate_count(text):
-    textLower=text.lower()
-    dupedList=[]
-    for i in range(len(textLower)):
-        countOfEachChar=textLower.count(textLower[i])
-        print(textLower[i], countOfEachChar)
-        if countOfEachChar>1 and dupedList.count(textLower[i])==0:
-            dupedList.append(textLower[i])
-    return len(dupedList)
-
-
-
-
-
 def square_sum(numbers):
     squaredArray=[n**2 for n in numbers]    
     square_sum=sum(squaredArray)
